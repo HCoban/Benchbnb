dfd48021a293b03687db0f019debec6edf5ce264
Halil Coban <halil.coban@gmail.com>
2016-08-26 14:53:59 -0700

    Bench model added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bench.create(lat: 37.767021, lng: -122.407740)
+Bench.create(lat: 37.771811, lng: -122.414221)
+Bench.create(lat: 37.770712, lng: -122.403054)
